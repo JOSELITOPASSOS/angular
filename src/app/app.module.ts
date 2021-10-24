545f81e86e8f95284a33244093789883fc384c50
unknown <joselitomartinho@gmail.com>
2021-10-24 20:38:44 -0300

    Aula 03 - Finalisado componente photo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { PhotoModule } from './photos/photos.module';
-import { PhotoComponet } from './photos/photo/photo.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { PhotoComponet } from './photo/photo.component';
 import { NgModule } from "@angular/core";
 
 @NgModule({
   declarations: [PhotoComponet],
+  imports: [HttpClientModule],
   exports: [PhotoComponet]
 })
 export class PhotoModule {
