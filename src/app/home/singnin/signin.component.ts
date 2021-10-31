7f8066feecf6f1c71889cdb2d1e8af4f3242d235
unknown <joselitomartinho@gmail.com>
2021-10-30 22:18:15 -0300

    Concluido componente da tela de login.

--- src/app/home/singnin/signin.component.ts
+++ src/app/home/singnin/signin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { PlatformDetectorService } from './../../core/plataform-detector/plataform-detector.service';
+import { AuthService } from './../../core/auth/auth.service';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -8,9 +11,15 @@ export class SigninComponent implements OnInit {
 
   loginForm!: FormGroup;
 
-  constructor(private formBuider: FormBuilder){
+  @ViewChild('userNameInput')
+  userNameInput!: ElementRef<HTMLInputElement>;
 
-  }
+  constructor(
+    private formBuider: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService
+    ){ }
 
   ngOnInit(): void {
     this.loginForm = this.formBuider.group({
@@ -19,5 +28,20 @@ export class SigninComponent implements OnInit {
     });
   }
 
+  login(){
+    const userName = this.loginForm.get('userName')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    this.authService.authenticate(userName, password).subscribe(
+      () => this.router.navigate(['user', userName]),
+      err => {
+        console.log(err);
+        this.loginForm.reset();
+        this.platformDetectorService.isPlatformBrowser() &&
+          this.userNameInput.nativeElement.focus();
+        alert('Invalid user name or password');
+      }
+    );
+  }
 
 }
