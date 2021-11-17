11ab5f3c1b6d33d00d6a6e9c0194a6ee455aedc4
unknown <joselitomartinho@gmail.com>
2021-11-17 20:35:22 -0300

    Add Inteceptor para enviar token.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,20 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule {
 
