8e7e9968ec759f5f022c38052e304ce26ddc0e8c
unknown <joselitomartinho@gmail.com>
2021-11-22 20:28:19 -0300

    Add upload de imagens.

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class PhotoService {
 
-  private API = 'http://localhost:3000';
+  private readonly API = 'http://localhost:3000';
   constructor(private http: HttpClient){}
 
   listFromUser(userName: string): Observable<Photo[]> {
@@ -16,5 +16,14 @@ export class PhotoService {
   listFromUserPaginated(userName: string, page: number){
     const params = new HttpParams().append('page', page.toString());
     return this.http.get<Photo[]>(`${this.API}/${userName}/photos`, {params: params});
+  }
+
+  upload(description: string, allowComments: boolean, file: File){
+    const formData = new FormData();
+    formData.append('description', description);
+    formData.append('allowComments', allowComments ? 'true' : 'false');
+    formData.append('imageFile', file);
+
+    return this.http.post(`${this.API}/photos/upload`, formData);
   }
 }
